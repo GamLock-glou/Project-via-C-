b2d4ee3837274d1b8c6dda004a350521bf8fa9a7
GamLock-glou <57964501+GamLock-glou@users.noreply.github.com>
2021-07-02 16:23:38 +0300

    Add files via upload

---
+++ fraction calculator/fraction_calculator/fraction_calculator/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace fraction_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.Divide = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.integer1 = new System.Windows.Forms.TextBox();
+            this.dividend1 = new System.Windows.Forms.TextBox();
+            this.divider1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.divider2 = new System.Windows.Forms.TextBox();
+            this.dividend2 = new System.Windows.Forms.TextBox();
+            this.integer2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.divider3 = new System.Windows.Forms.TextBox();
+            this.dividend3 = new System.Windows.Forms.TextBox();
+            this.integer3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(602, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Decision";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Plus
+            // 
+            this.Plus.Location = new System.Drawing.Point(91, 73);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(120, 34);
+            this.Plus.TabIndex = 1;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = true;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
+            // 
+            // Minus
+            // 
+            this.Minus.Location = new System.Drawing.Point(217, 73);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(115, 34);
+            this.Minus.TabIndex = 2;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            this.Minus.Click += new System.EventHandler(this.Minus_Click);
+            // 
+            // Multiply
+            // 
+            this.Multiply.Location = new System.Drawing.Point(428, 73);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(120, 34);
+            this.Multiply.TabIndex = 3;
+            this.Multiply.Text = "*";
+            this.Multiply.UseVisualStyleBackColor = true;
+            this.Multiply.Click += new System.EventHandler(this.Multiply_Click);
+            // 
+            // Divide
+            // 
+            this.Divide.Location = new System.Drawing.Point(554, 73);
+            this.Divide.Name = "Divide";
+            this.Divide.Size = new System.Drawing.Size(139, 34);
+            this.Divide.TabIndex = 4;
+            this.Divide.Text = "/";
+            this.Divide.UseVisualStyleBackColor = true;
+            this.Divide.Click += new System.EventHandler(this.Divide_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(280, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "+";
+            // 
+            // integer1
+            // 
+            this.integer1.Location = new System.Drawing.Point(91, 169);
+            this.integer1.Name = "integer1";
+            this.integer1.Size = new System.Drawing.Size(75, 22);
+            this.integer1.TabIndex = 10;
+            // 
+            // dividend1
+            // 
+            this.dividend1.Location = new System.Drawing.Point(175, 146);
+            this.dividend1.Name = "dividend1";
+            this.dividend1.Size = new System.Drawing.Size(75, 22);
+            this.dividend1.TabIndex = 11;
+            // 
+            // divider1
+            // 
+            this.divider1.Location = new System.Drawing.Point(175, 200);
+            this.divider1.Name = "divider1";
+            this.divider1.Size = new System.Drawing.Size(75, 22);
+            this.divider1.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(172, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "__________";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(396, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "__________";
+            // 
+            // divider2
+            // 
+            this.divider2.Location = new System.Drawing.Point(399, 199);
+            this.divider2.Name = "divider2";
+            this.divider2.Size = new System.Drawing.Size(75, 22);
+            this.divider2.TabIndex = 17;
+            // 
+            // dividend2
+            // 
+            this.dividend2.Location = new System.Drawing.Point(399, 145);
+            this.dividend2.Name = "dividend2";
+            this.dividend2.Size = new System.Drawing.Size(75, 22);
+            this.dividend2.TabIndex = 16;
+            // 
+            // integer2
+            // 
+            this.integer2.Location = new System.Drawing.Point(315, 168);
+            this.integer2.Name = "integer2";
+            this.integer2.Size = new System.Drawing.Size(75, 22);
+            this.integer2.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(496, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "=";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(605, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "__________";
+            // 
+            // divider3
+            // 
+            this.divider3.Location = new System.Drawing.Point(608, 202);
+            this.divider3.Name = "divider3";
+            this.divider3.ReadOnly = true;
+            this.divider3.Size = new System.Drawing.Size(75, 22);
+            this.divider3.TabIndex = 22;
+            // 
+            // dividend3
+            // 
+            this.dividend3.Location = new System.Drawing.Point(608, 148);
+            this.dividend3.Name = "dividend3";
+            this.dividend3.ReadOnly = true;
+            this.dividend3.Size = new System.Drawing.Size(75, 22);
+            this.dividend3.TabIndex = 21;
+            // 
+            // integer3
+            // 
+            this.integer3.Location = new System.Drawing.Point(524, 171);
+            this.integer3.Name = "integer3";
+            this.integer3.ReadOnly = true;
+            this.integer3.Size = new System.Drawing.Size(75, 22);
+            this.integer3.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(88, 385);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.divider3);
+            this.Controls.Add(this.dividend3);
+            this.Controls.Add(this.integer3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.divider2);
+            this.Controls.Add(this.dividend2);
+            this.Controls.Add(this.integer2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.divider1);
+            this.Controls.Add(this.dividend1);
+            this.Controls.Add(this.integer1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Divide);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Fraction calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button Divide;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox integer1;
+        private System.Windows.Forms.TextBox dividend1;
+        private System.Windows.Forms.TextBox divider1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox divider2;
+        private System.Windows.Forms.TextBox dividend2;
+        private System.Windows.Forms.TextBox integer2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox divider3;
+        private System.Windows.Forms.TextBox dividend3;
+        private System.Windows.Forms.TextBox integer3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ fraction calculator/fraction_calculator/fraction_calculator/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fraction_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Plus_Click(object sender, EventArgs e)
+        {
+            label1.Text = "+";
+        }
+
+        private void Minus_Click(object sender, EventArgs e)
+        {
+            label1.Text = "-";
+        }
+
+        private void Multiply_Click(object sender, EventArgs e)
+        {
+            label1.Text = "*";
+        }
+
+        private void Divide_Click(object sender, EventArgs e)
+        {
+            label1.Text = "/";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            condition();
+
+        }
+
+        void condition()
+        {
+            Chet c = new Chet(Convert.ToInt32(integer1.Text), Convert.ToInt32(integer2.Text),
+                Convert.ToInt32(dividend1.Text), Convert.ToInt32(dividend2.Text),
+                Convert.ToInt32(divider1.Text), Convert.ToInt32(divider1.Text));
+            (int,int,int) i = c.signs(label1.Text);
+            dividend3.Text = $"{i.Item1}";
+            divider3.Text = $"{i.Item2}";
+            integer3.Text = $"{i.Item3}";
+
+            if (integer3.Text == "0")
+                integer3.Text = "";
+            if (dividend3.Text == "0")
+            {
+                dividend3.Text = "";
+                divider3.Text = "";
+            }
+
+
+            /*
+            try
+            {
+                int i_n1 = Convert.ToInt32(integer1.Text), i_n2 = Convert.ToInt32(integer2.Text); //Целые числа
+                int dev_d1 = Convert.ToInt32(dividend1.Text), dev_d2 = Convert.ToInt32(dividend2.Text);//Делимые
+                int dev_r1 = Convert.ToInt32(divider1.Text), dev_r2 = Convert.ToInt32(divider2.Text); //Делители
+                int dev_r_CA = dev_r1 * dev_r2; //Делитель ср_ар
+                int SM_dev_d1 = (i_n1 * dev_r1 + dev_d1) * dev_r2; //Делимое 1 (for + and -)
+                int SM_dev_d2 = (i_n2 * dev_r2 + dev_d2) * dev_r1; //Делимое 2 (for + and -)
+                //____________________________________
+                int M_dev_d = (i_n1 * dev_r1 + dev_d1) * (i_n2 * dev_r2 + dev_d2);
+                int M_dev_r = dev_r1 * dev_r2;
+                //____________________________________
+                int D_dev_d = (i_n1 * dev_r1 + dev_d1) * dev_r2;
+                int D_dev_r = (i_n2 * dev_r2 + dev_d2) * dev_r1;
+                //____________________________________
+                if (label1.Text == "+")
+                {
+                    dividend3.Text = $"{(SM_dev_d1 + SM_dev_d2) % (dev_r_CA)}";
+                    divider3.Text = $"{dev_r_CA}";
+                    integer3.Text = $"{(SM_dev_d1 + SM_dev_d2) / dev_r_CA}";
+                }
+                else
+                if (label1.Text == "-")
+                {
+                    dividend3.Text = $"{(SM_dev_d1 - SM_dev_d2) % dev_r_CA}";
+                    divider3.Text = $"{dev_r_CA}";
+                    integer3.Text = $"{(SM_dev_d1 - SM_dev_d2) / dev_r_CA}";
+                }
+                else
+                if (label1.Text == "*")
+                {
+                    dividend3.Text = $"{M_dev_d % M_dev_r}";
+                    divider3.Text = $"{M_dev_r}";
+                    integer3.Text = $"{(M_dev_d) / M_dev_r}";
+                }
+                else
+                if (label1.Text == "/")
+                {
+                    dividend3.Text = $"{D_dev_d % D_dev_r}";
+                    divider3.Text = $"{D_dev_r}";
+                    integer3.Text = $"{D_dev_d / D_dev_r}";
+                }
+                if (integer3.Text == "0")
+                    integer3.Text = "";
+            }
+            catch (Exception)
+            { label6.Text = "Ошибка: Заполните все ячейки!"; } */
+        }
+
+    }
+
+    class Chet
+    {
+        public int integer1 { get; set; }
+        public int integer2 { get; set; }
+
+
+        public int dividend1 { get; set; }
+        public int dividend2 { get; set; }
+
+        public int divider1 { get; set; }
+        public int divider2 { get; set; }
+
+
+        public Chet(int integer1, int integer2, int dividend1, int dividend2,
+            int divider1, int divider2)
+        {
+            this.integer1 = integer1;
+            this.integer2 = integer2;
+            this.dividend1 = dividend1;
+            this.dividend2 = dividend2;
+            this.divider1 = divider1;
+            this.divider2 = divider2;
+        }
+
+        public (int, int, int) signs(string s)
+        {
+            if (s == "+")
+                return sum();
+            else if (s == "-")
+                return den();
+            else if (s == "*")
+                return mult();
+            else if (s == "/")
+                return diff();
+            else
+                return (0,0,0);
+        }
+        public (int, int, int) sum()
+        {
+            int dev_r_CA = divider1 * divider2; //Делитель ср_ар
+            int SM_dev_d1 = (integer1 * divider1 + dividend1) * divider2; //Делимое 1 (for + and -)
+            int SM_dev_d2 = (integer2 * divider2 + dividend2) * divider1; //Делимое 2 (for + and -)
+            return ((SM_dev_d1 + SM_dev_d2) % (dev_r_CA), dev_r_CA, (SM_dev_d1 + SM_dev_d2) / dev_r_CA);
+        }
+
+        public (int, int, int) den()
+        {
+            int dev_r_CA = divider1 * divider2; //Делитель ср_ар
+            int SM_dev_d1 = (integer1 * divider1 + dividend1) * divider2; //Делимое 1 (for + and -)
+            int SM_dev_d2 = (integer2 * divider2 + dividend2) * divider1; //Делимое 2 (for + and -)
+            return ((SM_dev_d1 - SM_dev_d2) % dev_r_CA, dev_r_CA, (SM_dev_d1 - SM_dev_d2) / dev_r_CA);
+        }
+
+        public (int, int, int) mult()
+        {
+            int M_dev_d = (integer1 * divider1 + dividend1) * (integer2 * divider2 + dividend2);
+            int M_dev_r = divider1 * divider2;
+            return (M_dev_d % M_dev_r, M_dev_r, (M_dev_d) / M_dev_r);
+        }
+
+        public (int, int, int) diff()
+        {
+            int D_dev_d = (integer1 * divider1 + dividend1) * divider2;
+            int D_dev_r = (integer2 * divider2 + dividend2) * divider1;
+            return (D_dev_d % D_dev_r, D_dev_r, D_dev_d / D_dev_r);
+        }
+    }
+}
